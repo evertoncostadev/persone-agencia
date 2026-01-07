23f6cea3be0885bb620c30927260ed9f1ed3f1c0
evertoncostadev <everton.jose@aluno.uepb.edu.br>
2026-01-07 15:08:37 -0300

    corrigindo chaves

--- persone-agencia/src/lib/supabaseClient.ts
+++ persone-agencia/src/lib/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Substitua pela SUA URL do Supabase (Project Settings > API)
-const supabaseUrl = 'sb_publishable_pj-kemcWr0ADuAiulKI0XQ_wgedQ1kT'
+const supabaseUrl = 'https://qhpcqebnnktsmweixvbm.supabase.co'
 
 // Substitua pela SUA CHAVE ANON (Project Settings > API)
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFocGNxZWJubmt0c213ZWl4dmJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc3MzA1MzgsImV4cCI6MjA4MzMwNjUzOH0.zhBWwd6_oLmRkXOSQUB3xTCEYOJA68O58gPGfxrjrXg'
